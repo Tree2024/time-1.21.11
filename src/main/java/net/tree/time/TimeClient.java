c48335ad2e8c0e1f3ec79824e48db0981de9430d
tree2024 <prestons.tcs@gmail.com>
2025-12-29 16:56:43 -0500

    Initial commit

---
+++ src/main/java/net/tree/time/TimeClient.java
@@ -0,0 +1,10 @@
+package net.tree.time;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TimeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/tree/time/TimeDataGenerator.java
@@ -0,0 +1,11 @@
+package net.tree.time;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TimeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
